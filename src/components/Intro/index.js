2c5a7243a6c406c9b73859cb72e0256240d2f48f
mdskrumi-bs23 <sakibul.alam@brainstation-23.com>
2022-01-02 01:01:00 +0600

    Add intro css

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,5 +1,10 @@
 const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    <div className="intro__container">
+      <div className="intro_img">Image</div>
+      <div className="intro_details">Intros</div>
+    </div>
+  );
 };
 
 export default Intro;
