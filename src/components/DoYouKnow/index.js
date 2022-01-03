230fb04ed6ef43f953f7e167bb0408168319b921
mdskrumi-bs23 <sakibul.alam@brainstation-23.com>
2022-01-03 15:55:55 +0600

    Add hm audio

--- src/components/DoYouKnow/index.js
+++ src/components/DoYouKnow/index.js
@@ -6,13 +6,24 @@ import DoYouKnowModal from "../DoYouKnowModal";
 import AE from "../../assets/images/ae.png";
 import { useState } from "react";
 
+// Files
+import hmAudio from "../../assets/files/hm.mp3";
+
 const DoYouKnow = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const audio = new Audio(hmAudio);
 
   return (
     <>
       {!isModalOpen ? (
-        <div className="do_you_know" onClick={() => setIsModalOpen(true)}>
+        <div
+          className="do_you_know"
+          onClick={() => {
+            audio.play();
+            setIsModalOpen(true);
+          }}
+        >
           <img src={AE} alt="Do You Know?" />
         </div>
       ) : null}
