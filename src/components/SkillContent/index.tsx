56c94efcc919d2b60952dcf81d804f5c40972181
Md Sakibul Alam <sakibul.alam@brainstation-23.com>
2022-02-14 01:30:58 +0600

    Add animation in to skills

--- src/components/SkillContent/index.tsx
+++ src/components/SkillContent/index.tsx
@@ -1,12 +1,43 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 const SkillContent = (props: any) => {
+  gsap.registerPlugin(ScrollTrigger);
   const { title, image } = props;
 
+  const imageDivRef = useRef<HTMLDivElement>(null);
+  const descriptionDivRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    gsap.from(imageDivRef.current, {
+      scrollTrigger: {
+        trigger: ".skill_container",
+        start: "top center",
+        end: "+=300",
+        scrub: 1,
+      },
+      x: -window.screen.availWidth,
+      duration: 2,
+    });
+    gsap.from(descriptionDivRef.current, {
+      scrollTrigger: {
+        trigger: ".skill_container",
+        start: "top center",
+        end: "+=300",
+        scrub: 1,
+      },
+      x: window.screen.availWidth,
+      duration: 2,
+    });
+  }, []);
+
   return (
-    <div className="skill_content_container">
-      <div className="image">
+    <div className={`skill_content_container ${title}`}>
+      <div className="image" ref={imageDivRef}>
         <img src={image} alt={title} />
       </div>
-      <div className="description">
+      <div className="description" ref={descriptionDivRef}>
         <div className="title">{title}</div>
         {/* <div className="skill_short_des"></div> */}
       </div>
