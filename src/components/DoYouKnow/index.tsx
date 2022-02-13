f218e4554716afd6bb634efe391e65b4f099e8fe
Md Sakibul Alam <sakibul.alam@brainstation-23.com>
2022-02-14 00:29:23 +0600

    Add einestine animation

--- src/components/DoYouKnow/index.tsx
+++ src/components/DoYouKnow/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import gsap from "gsap";
+
+import Draggable from "gsap/Draggable";
 
 // Custom Components
 import Modal from "../Modal";
@@ -8,11 +11,36 @@ import DoYouKnowModal from "../DoYouKnowModal";
 import AE from "../../assets/images/ae.png";
 
 const DoYouKnow = () => {
+  gsap.registerPlugin(Draggable);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const imageDivRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    Draggable.create(".do_you_know", {
+      type: "x,y",
+      inertia: true,
+      onDragEnd: function () {
+        console.log(
+          "x velocity is: " +
+            InertiaPlugin.getVelocity(this.target, "x") +
+            " and the duration is " +
+            this.tween.duration() +
+            " seconds."
+        );
+      },
+    });
+    gsap.timeline().from(imageDivRef.current, {
+      x: -window.screen.availWidth,
+      duration: 2,
+    });
+  }, []);
+
   return (
     <>
       {!isModalOpen ? (
         <div
+          ref={imageDivRef}
           className="do_you_know"
           onClick={() => {
             setIsModalOpen(true);
