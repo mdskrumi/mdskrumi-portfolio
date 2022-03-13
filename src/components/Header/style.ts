fe6d3d2d48fe2a1a1c8d9958503aa71c7efc0100
Md Sakibul Alam <sakibul.alam@brainstation-23.com>
2022-03-13 18:42:19 +0600

    Fix home sc

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -30,6 +30,7 @@ export const HeaderLeftImageDiv = styled.div`
     display: block;
     text-align: center;
     width: 100vw;
+    padding-bottom: 5px;
   }
 `;
 
@@ -43,6 +44,12 @@ export const HeaderLeftTitleDiv = styled.div`
   font-size: 24px;
   margin: 0px 0px 0px 0.75vw;
   cursor: pointer;
+
+  &:hover {
+    color: #ffffff;
+    text-shadow: 0px 0px 24px #000000, 0px 0px 24px #000000;
+    font-family: "Lobster", cursive;
+  }
 
   @media (max-width: 600px) {
     font-size: 26px;
@@ -66,4 +73,9 @@ export const HeaderRightUl = styled.ul`
 
 export const HeaderRightLi = styled.li`
   width: 80px;
+  &:hover {
+    color: #ffffff;
+    text-shadow: 0px 0px 24px #000000, 0px 0px 24px #000000;
+    font-family: "Lobster", cursive;
+  }
 `;
--- src/containers/Home/style.ts
+++ src/containers/Home/style.ts
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const HomeContainerDiv = styled.div`
   padding-bottom: 0vh;
+
+  @media (max-width: 600px) {
+    padding-bottom: 15vh;
+  }
 `;
